e3bba9731b90bf2bebabff9c123d800e20c82fd6
Syahir06 <s22a0034@siswa.umk.edu.my>
2026-01-30 00:02:22 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -12,3 +12,21 @@ def clean_text(text):
     words = text.split()
     words = [w for w in words if w not in stop_words]
     return " ".join(words)
+
+from src.preprocessing import clean_text
+
+df['clean_text'] = df['text'].apply(clean_text)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer(max_features=3000)
+
+X = vectorizer.fit_transform(df['clean_text'])
+y = df['airline_sentiment']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
