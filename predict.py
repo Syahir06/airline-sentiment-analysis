fa784c3ad9d57bfdca9b55c55f81c4f9364a01fb
Syahir06 <s22a0034@siswa.umk.edu.my>
2026-01-30 00:30:20 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,12 +1,12 @@
 import pickle
-from preprocessing import clean_text
 
 model = pickle.load(open("../model.pkl", "rb"))
 vectorizer = pickle.load(open("../vectorizer.pkl", "rb"))
 
 def predict_sentiment(text):
-    text = clean_text(text)
     text_vec = vectorizer.transform([text])
     return model.predict(text_vec)[0]
 
-print(predict_sentiment("The flight was delayed and terrible"))
+# Test
+print(predict_sentiment("The flight was amazing!"))
+print(predict_sentiment("Worst airline ever"))
