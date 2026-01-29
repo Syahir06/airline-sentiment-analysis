7cb7ead0677aec157984af036de01e1442fc6cf8
Syahir06 <s22a0034@siswa.umk.edu.my>
2026-01-30 00:19:51 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -6,27 +6,9 @@ nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
 def clean_text(text):
-    text = text.lower()
-    text = re.sub(r"http\S+", "", text)  # remove URLs
-    text = re.sub(r"[^a-zA-Z]", " ", text)  # remove symbols
+    text = str(text).lower()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^a-zA-Z]", " ", text)
     words = text.split()
     words = [w for w in words if w not in stop_words]
     return " ".join(words)
-
-from src.preprocessing import clean_text
-
-df['clean_text'] = df['text'].apply(clean_text)
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-vectorizer = TfidfVectorizer(max_features=3000)
-
-X = vectorizer.fit_transform(df['clean_text'])
-y = df['airline_sentiment']
-
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
